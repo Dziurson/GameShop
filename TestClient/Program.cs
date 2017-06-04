efa831cc08120ce29fc1beb7e5094a5e116198de
Jakub Dziwura <jakub.dziwura@gmail.com>
2017-06-05 00:54:09 +0200

    Dodano testowego klienta.

---
+++ TestClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using EFGameShopDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestClient.GameShopWarehouseService;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameShopWarehouseClient client = new GameShopWarehouseClient();
+            foreach (Item item in client.GetAllItems())
+            {
+                Console.WriteLine(item.name + " " + item.description);
+            }
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
